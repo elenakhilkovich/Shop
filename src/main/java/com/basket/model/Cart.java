e7f5c4eb924d9815866916cc3362e81b20412587
Елена Хилькович <elenakhilkovich@gmail.com>
2022-02-06 23:03:45 +0300

    lesson 2.11

--- src/main/java/com/basket/model/Cart.java
+++ src/main/java/com/basket/model/Cart.java
@@ -1,6 +1,7 @@
 package com.basket.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cart {
@@ -8,12 +9,12 @@ public class Cart {
     private final List<Integer> userCurrentBarcodeList = new ArrayList<>();
 
     public List<Integer> getUserCurrentBarcodeList() {
-        return userCurrentBarcodeList;
+        return Collections.unmodifiableList(userCurrentBarcodeList);
     }
 
     public List<Integer> addBarcode(List<Integer> newBarcodes) {
         userCurrentBarcodeList.addAll(newBarcodes);
-        return userCurrentBarcodeList;
+        return newBarcodes;
     }
 }
 
